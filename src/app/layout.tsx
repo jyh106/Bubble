e34cf8a361f732b6a2154412d23fd92c066a69aa
Jiayao Huang <jyh106@berkeley.edu>
2023-06-11 17:13:05 -0700

    mvp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@
 
 import './globals.css'
 import { Fuzzy_Bubbles } from 'next/font/google'
-import Link from 'next/link';
 import { v4 } from "uuid";
+import {useState} from 'react'; 
 
 const fuzzyBubbles = Fuzzy_Bubbles({ weight: '700' })
 
@@ -20,8 +20,9 @@ const mdBubbles = bubbliness*3;
 const lgBubbles = bubbliness*2; 
 
 type BubbleLayer = 'layer1' | 'layer2' | 'layer3' | 'layer4';
+type BubbleSize = 'bubble-xs' | 'bubble-sm' | 'bubble-md' | 'bubble-lg';
 
-const layerBubbles: { [key: string]: { size: string, bubbliness: number }} = {
+const layerBubbles: { [key: string]: { size: BubbleSize, bubbliness: number }} = {
   'layer1': {size: 'bubble-xs', bubbliness: xsBubbles},
   'layer2': {size: 'bubble-sm', bubbliness: smBubbles},
   'layer3': {size: 'bubble-md', bubbliness: mdBubbles},
@@ -31,45 +32,78 @@ const layerBubbles: { [key: string]: { size: string, bubbliness: number }} = {
 const bubbleWobbleClassname = 'bubble-wobble';
 const bubblePopClassname = 'bubble-pop';
 
-const onMouseOverBubble = (bubbleElementId: string) => {
-  const bubbleElement = document.getElementById(bubbleElementId);
-  console.log('mouse over bubble', bubbleElementId);
-  if (bubbleElement?.classList.contains(bubbleWobbleClassname)) {
-    bubbleElement.classList.add(bubblePopClassname);
-    setTimeout(() => bubbleElement.classList.add('hide'), 500);
-  } else {
-    bubbleElement?.classList.add(bubbleWobbleClassname);
-  }
-  console.log(bubbleElement?.classList);
-};
 
-const fillBubbleLayer = (layer: BubbleLayer) => {
-  const bubbles = [];
-  for(var i = 0; i < layerBubbles[layer].bubbliness; i++) {
-    const topPos = `${Math.random() * 100}%`;
-    const leftPos = `${Math.random() * 100}%`;
-    const id = v4();
-    bubbles.push(
-      <div 
-        className={`bubble ${layerBubbles[layer].size}`} 
-        style={{top: topPos, left: leftPos}}
-        onMouseOver={(e) => onMouseOverBubble(e.currentTarget.id)}
-        id={id}
-      />
-    );
-  }
-  return bubbles;
-
-}
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const beat = new Audio('bubble-pop-sound.mp3');
+  const [mutedByUser, setMutedByUser] = useState(false);
+  beat.muted = true;
+
+  const onMouseOverBubble = (bubbleElementId: string) => {
+    const bubbleElement = document.getElementById(bubbleElementId);
+    if (bubbleElement?.classList.contains(bubbleWobbleClassname)) {
+      if (beat.muted && !mutedByUser) {
+        beat.muted = false;
+      };
+      !beat.muted && beat.play();
+      bubbleElement.classList.add(bubblePopClassname);
+      setTimeout(() => bubbleElement.classList.add('hide'), 500);
+    } else {
+      bubbleElement?.classList.add(bubbleWobbleClassname);
+    }
+  };
+  
+  const fillBubbleLayer = (layer: BubbleLayer) => {
+    const bubbles = [];
+    for(var i = 0; i < layerBubbles[layer].bubbliness; i++) {
+      const topPos = `${Math.random() * 100}%`;
+      const leftPos = `${Math.random() * 100}%`;
+      const id = v4();
+      bubbles.push(
+        <div 
+          className={`bubble ${layerBubbles[layer].size}`} 
+          style={{top: topPos, left: leftPos}}
+          onMouseOver={(e) => onMouseOverBubble(e.currentTarget.id)}
+          id={id}
+          key={id}
+        />
+      );
+    }
+    return bubbles;
+  }
+
+  // Mute icon by https://icons8.com
+  const renderMuteButton = () => {
+    if (mutedByUser) {
+      return (
+        <img 
+          src="icons8-mute-64.png" 
+          alt="Mute_Icon.svg" 
+          title="Mute icon" 
+          onClick={() => setMutedByUser(false)}
+        />
+      )
+      } else {
+        return (
+          <img 
+            src="icons8-speaker-64.png" 
+            alt="Speaker_Icon.svg" 
+            title="Unmute/speaker icon" 
+            onClick={() => setMutedByUser(true)}
+          />
+        )
+    }
+  };
 
   return (
     <html lang="en">
       <body className={fuzzyBubbles.className}>
+        <div className='buttonContainer'>
+          {renderMuteButton()}
+        </div>
         <div className='layer layer1'>
           {fillBubbleLayer('layer1')}
         </div>
@@ -82,14 +116,6 @@ export default function RootLayout({
         <div className='layer layer4'>
           {fillBubbleLayer('layer4')}
         </div>
-        {/* <nav>
-            <Link href="/">
-              Home
-            </Link>
-            <Link href="/notes">
-              Notes
-            </Link>
-          </nav> */}
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,96 @@
 'use client';
 
-import { ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useState} from "react";
+
+const RantBubbleHeightDefault = 100;
+const RantBubbleWidthDefault = 100;
+
+function randomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const debounce = (fn: Function, ms = 300) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
 
 export default function Home() {
+  const [topLayerBubbles, setTopLayerBubbles] = useState<HTMLElement[]>();
+  const [rantLength, setRantLength] = useState(0);
+  const [rantBubbleHeight, setRantBubbleHeight] = useState(RantBubbleHeightDefault);
+  const [rantBubbleWidth, setRantBubbleWidth] = useState(RantBubbleWidthDefault);
+  const [selectedRantBubble, setSelectedRantBubble] = useState<HTMLElement>();
+  
+  const beat = new Audio('bubble-pop-sound.mp3');
+
+  let oldValue = 0;
   const onRanting = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const newValue = e.target.value;
-    console.log(newValue.length);
+    const newValueLength = e.target.value.length;
+    setRantLength(newValueLength - oldValue);
+    oldValue = newValueLength;
   };
+
+  useEffect(() => {
+    const bubbles = Array.from(document.getElementsByClassName('bubble') as HTMLCollectionOf<HTMLElement>);
+    const selectedBubble = bubbles[randomNumber(0, bubbles.length)];
+    setTopLayerBubbles(bubbles);
+    setSelectedRantBubble(selectedBubble);
+    selectedBubble.style.width = `${rantBubbleWidth}px`;
+    selectedBubble.style.height = `${rantBubbleHeight}px`;
+  }, []);
+
+  useEffect(() => {
+    setRantBubbleHeight(rantBubbleHeight + (rantLength * 0.02));
+    setRantBubbleWidth(rantBubbleWidth + (rantLength * 0.02));
+
+    const pumpBubble = () => {
+      if (!selectedRantBubble) {
+        return;
+      }
+      selectedRantBubble.style.width = `${rantBubbleWidth}px`;
+      selectedRantBubble.style.height = `${rantBubbleHeight}px`;
+
+      if (Number(selectedRantBubble.style.opacity) < 1 && (rantBubbleHeight < 200)) {
+        selectedRantBubble.style.opacity = `${Number(selectedRantBubble.style.opacity) + 0.1}`;
+        if (Number(selectedRantBubble.style.zIndex) < 1) {
+          selectedRantBubble.style.zIndex = '1';
+        }
+      }
+    };
+
+    const selectNewBubble = () => {
+      setRantBubbleHeight(RantBubbleHeightDefault);
+      setRantBubbleWidth(RantBubbleWidthDefault);
+      topLayerBubbles && setSelectedRantBubble(topLayerBubbles[randomNumber(0, topLayerBubbles.length)]);
+    };
+
+    const popBubble = () => {
+      if (!selectedRantBubble) {
+        return;
+      }
+      console.log('called pop');
+      
+      beat.play();
+      console.log('played');
+      selectedRantBubble.classList.add('hide');
+
+      selectNewBubble();
+    };
+
+    pumpBubble();
+
+    const debouncePop = debounce(popBubble, 410);
+
+    if (selectedRantBubble && rantBubbleHeight > 450 && !selectedRantBubble?.classList.contains('bubble-pop')) {
+      selectedRantBubble.classList.add('rant-bubble-wobble');
+      selectedRantBubble.classList.add('bubble-pop');
+      debouncePop();
+    }
+
+  }, [rantLength]);
 
   return (
    <div>
@@ -18,8 +102,6 @@ export default function Home() {
           onChange={onRanting}
         />
       </div>
-      {/* <div className="ball"></div> */}
-      {/* <div className="shadow"></div> */}
     </main>
    </div>
   )
